c2cd7f485ab05d17fab096ae9614ef8f6d4939ac
Fanasina95 <159781294+Fanasina95@users.noreply.github.com>
2024-03-24 22:41:28 +0300

    Create base.sql
    hihi

---
+++ base.sql
@@ -0,0 +1,11 @@
+create database organigrame;
+use organigrame;
+
+
+CREATE TABLE Employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pdi INT,   
+    titre VARCHAR(255),
+    nom VARCHAR(255)    
+);
+
